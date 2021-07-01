f48efb839679b65dcf66836e748f6821cdcb07b3
caiovinics <caiovinics@users.noreply.github.com>
2021-07-01 15:50:01 -0400

    ajuste na api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './styles/global.scss'
 import Header from './components/Header/Header'
 import Routes from './routes';
-//import Main from './pages/main/main'
 
 
 
--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://desafio-123projetei-node.herokuapp.com'
+  baseURL: 'https://desafio-123projetei-node.herokuapp.com'
 });
 
 export default api;
